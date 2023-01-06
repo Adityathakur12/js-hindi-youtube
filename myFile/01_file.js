ba2018e5a8450e5e0132aaa480d111e62246f947
Adityathakur12 <114096753+Adityathakur12@users.noreply.github.com>
2023-01-06 07:35:09 +0000

    my file

---
+++ myFile/01_file.js
@@ -0,0 +1,11 @@
+// array in javascript
+
+const arr=[2,3,4,5] //decleration of array
+
+console.log(arr[0]); // acessing array element
+
+// acessing array element through for loop
+
+for(let i=0;i<4-1;i++){
+    console.log(arr[i]);
+}
